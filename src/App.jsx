0d513e5eedd225b031591b80c21cf79908e5216c
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-12-25 23:35:36 +0600

    about page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Parallax from "./components/parallax/Parallax";
+import About from "./components/about/About";
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <Hero />
       </section>
       <section id="About"><Parallax type="about"/></section>
-      <section>About</section>
+      <section><About /></section>
       <section id="Education"><Parallax type="education"/></section>
       <section>Education</section>
       <section>Portfolio1</section>
---
+++ src/components/about/About.jsx
@@ -0,0 +1,11 @@
+import "./about.scss"
+
+const About = () => {
+    return (
+        <div className="about">
+            This is about page
+        </div>
+    );
+};
+
+export default About;
