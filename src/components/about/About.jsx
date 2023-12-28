e40a81f46c09e4b3996b957ce8922eeb74b2e4a7
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-12-28 17:27:15 +0600

    about section designed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,26 +1,24 @@
 import "./about.scss";
-import sabbir from "../../assets/sabbir.png";
+import sabbir from "../../assets/sabbir2.png";
 
 const About = () => {
   return (
     <div className="about">
-      <div className="header">
-        About <span>Me</span>
-      </div>
       <div className="box">
+        <div className="header">
+          About <span>Me</span>
+        </div>
         <img src={sabbir} alt="" />
+        <h1>Full Stack Web Developer</h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias omnis
-          eligendi ex aut rerum, rem tenetur error quaerat optio molestias at
-          odio quasi magnam obcaecati nesciunt sunt neque consequuntur quidem
-          vero corrupti, deleniti accusantium non voluptate! Dolores laborum,
-          quo accusamus beatae incidunt eum ratione dignissimos deserunt maiores
-          voluptas placeat asperiores! Id, unde voluptates, velit dignissimos
-          officia tempore fugit suscipit corrupti excepturi facilis libero ad
-          odit molestias harum possimus sequi cupiditate beatae reprehenderit
-          ipsum iste. Reiciendis dolor laborum modi eum perferendis tempore
-          rerum, hic ullam deleniti autem suscipit obcaecati sit? Molestiae et
-          nihil eveniet beatae alias, aperiam laborum iste saepe distinctio.
+          Hi, I'm a MERN Full Stack Web Developer. I like building
+          websites using MongoDB, Express.js, React, and Node.js. I find joy in
+          solving problems, especially when working together with others. Making
+          websites that people find easy and enjoyable to use is my goal. I have
+          experience creating different parts of websites, both the visible
+          parts that users see and the behind-the-scenes stuff. I'm excited to
+          be part of projects that are creative and innovative. Let's team up
+          and bring some awesome ideas to life on the web!
         </p>
       </div>
     </div>
