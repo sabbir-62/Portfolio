037671baf6c8f5c64ad9a6c79d73a8eacf2baeb8
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-12-30 17:28:13 +0600

    [In progress] portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,55 +1,76 @@
 import { useRef } from "react";
-import "./portfolio.scss"
-import {motion, useScroll, useSpring } from 'framer-motion';
-
+import "./portfolio.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 const items = [
-    {
-        id: 1,
-        title: "Blog Website",
-        image: "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-        description: "This is my blog website"
-    },
-    {
-        id: 2,
-        title: "HSTU Alumni Website",
-        image: "https://images.unsplash.com/photo-1527525443983-6e60c75fff46?q=80&w=985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        description: "This is my alumni website"
-    }
+  {
+    id: 1,
+    title: "Blog Website",
+    image:
+      "https://images.pexels.com/photos/262508/pexels-photo-262508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    description: "This is my blog website",
+  },
+  {
+    id: 2,
+    title: "HSTU Alumni Website",
+    image:
+      "https://images.unsplash.com/photo-1527525443983-6e60c75fff46?q=80&w=985&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    description: "This is my alumni website",
+  },
 ];
 
+const Item = ({ item }) => {
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    // offset: ["start start", "end start"]
+  });
 
-const Item = ({item}) => {
-    return (
-        <section>
-            {item.title}
-        </section>
-    )
-}
+  const y = useTransform(scrollYProgress, [0, 1], [-300, 300]);
+
+  return (
+    <section className="portfolioSection">
+      <div className="container">
+        <div className="wrapper">
+          <div className="imageContainer" ref={ref}>
+            <img src={item.image} alt="Portfolio image" />
+          </div>
+          <motion.div className="textContainer" style={{y: y}}>
+            <h2>{item.title}</h2>
+            <p>{item.description}</p>
+          </motion.div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 const Portfolio = () => {
-    const ref = useRef()
-    const {scrollYProgress} = useScroll({
-        target: ref,
-        offset: ["end end", "start start"]
-    })
+  const ref = useRef();
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["end end", "start start"],
+  });
 
-    const scaleX = useSpring(scrollYProgress,{
-        stiffness: 100,
-        damping: 100
-    })
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 100,
+  });
 
-    return (
-        <div className="portfolio" ref={ref}>
-            <div className="progress">
-                <h1>My Works</h1>
-                <motion.div style={{scaleX: scaleX}} className="progressBar"></motion.div>
-            </div>
-            {items.map(item => (
-                <Item item={item} key={item.id}/>
-            ))}
-        </div>
-    );
+  return (
+    <div className="portfolio" ref={ref}>
+      <div className="progress">
+        <h1>My Works</h1>
+        <motion.div
+          style={{ scaleX: scaleX }}
+          className="progressBar"
+        ></motion.div>
+      </div>
+      {items.map((item) => (
+        <Item item={item} key={item.id} />
+      ))}
+    </div>
+  );
 };
 
 export default Portfolio;
