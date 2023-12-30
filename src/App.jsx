5fb8679ba777fb323a66e2db665eb88d92d27f45
sabbir-62 <sabbir.hossain.se62@gmail.com>
2023-12-30 23:56:22 +0600

    contact page created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import About from "./components/about/About";
 import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
       <section id="Education">Education</section>
       <section id="Skills">Skills</section>
       <section id="Works"><Portfolio /></section>
-      <section id="Contact">Contact</section>
+      <section id="Contact"><Contact /></section>
     </div>
   );
 }
---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,11 @@
+import "./contact.scss"
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            Contact page
+        </div>
+    );
+};
+
+export default Contact;
