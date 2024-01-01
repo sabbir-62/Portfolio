cebb9ccc3d4cac48224dad34503a12b8cd6673e6
sabbir-62 <sabbir.hossain.se62@gmail.com>
2024-01-01 09:13:51 +0600

    circle cursor added

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Hero from "./components/hero/Hero";
 import About from "./components/about/About";
 import Portfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 
 function App() {
   return (
     <div>
+      <Cursor />
       <section id="Home">
         <Navbar />
         <Hero />
