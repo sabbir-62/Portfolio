45bbeb5cf25521ca3f56a343ad5c7610cd48507f
sabbir-62 <sabbir.hossain.se62@gmail.com>
2024-01-11 01:00:41 +0600

    [In Progress] blog details page design

--- src/components/portfolio/projectDetails/BlogProjectDetails.jsx
+++ src/components/portfolio/projectDetails/BlogProjectDetails.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
+import blog1 from "../../../assets/blog/blog1.png";
+import blog2 from "../../../assets/blog/blog2.png";
+import blog3 from "../../../assets/blog/blog3.png";
+import blog4 from "../../../assets/blog/blog4.png";
+import blog5 from "../../../assets/blog/blog5.png";
+import "./projectDetails.scss";
 
 const BlogProjectDetails = () => {
-    return (
-        <div>
-            Blog Project
+  return (
+    <div className="blog-container">
+      <div className="image-container">
+        <div className="img">
+          <img src={blog1} alt="Blog pic" />
         </div>
-    );
+        <div className="img">
+          <img src={blog2} alt="Blog pic" />
+        </div>
+        <div className="img bind-image">
+          <img src={blog3} alt="Blog pic" />
+          <img src={blog4} alt="Blog pic" />
+        </div>
+        <div className="img">
+          <img src={blog5} alt="Blog pic" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default BlogProjectDetails;
