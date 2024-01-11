cd6b5cfea5de85fb8b71f0e13b1cf9fe1b63faa8
sabbir-62 <sabbir.hossain.se62@gmail.com>
2024-01-11 22:57:42 +0600

    key features added to the blog details page

--- src/components/portfolio/projectDetails/BlogProjectDetails.jsx
+++ src/components/portfolio/projectDetails/BlogProjectDetails.jsx
@@ -4,23 +4,56 @@ import blog3 from "../../../assets/blog/blog3.png";
 import blog4 from "../../../assets/blog/blog4.png";
 import blog5 from "../../../assets/blog/blog5.png";
 import "./projectDetails.scss";
+import { motion } from 'framer-motion';
 
 const BlogProjectDetails = () => {
+
+  const textVariant = {
+    initial: {
+        x: -200,
+        opacity: 0
+    },
+    animate:{
+        x: 0,
+        opacity: 1,
+        transition:{
+            duration: 1,
+            staggerChildren: 0.1
+        }
+    }
+}
+
+
   return (
     <div className="blog-container">
       <div className="image-container">
-        <div className="img">
+        <motion.div className="img"  variants={textVariant} initial="initial" whileInView="animate">
           <img src={blog1} alt="Blog pic" />
-        </div>
-        <div className="img">
+        </motion.div>
+        <motion.div className="img" variants={textVariant} initial="initial" whileInView="animate">
           <img src={blog2} alt="Blog pic" />
-        </div>
-        <div className="img bind-image">
+        </motion.div>
+        <motion.div className="img bind-image" variants={textVariant} initial="initial" whileInView="animate">
           <img src={blog3} alt="Blog pic" />
           <img src={blog4} alt="Blog pic" />
-        </div>
-        <div className="img">
+        </motion.div>
+        <motion.div className="img" variants={textVariant} initial="initial" whileInView="animate">
           <img src={blog5} alt="Blog pic" />
+        </motion.div>
+      </div>
+
+      <div className="text-container">
+        <div className="key-features">
+          <motion.h1 variants={textVariant} initial="initial" whileInView="animate">Key <span>Features</span></motion.h1>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>1.</span> Users can registration and login</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>2.</span> After submitting user data for registration, an otp sent into user email</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>3.</span> User need to verified this otp for completing registration process</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>4.</span> After successful registration, user need to login</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>5.</span> After login user can show all blogs</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>6.</span> Users read others blog details</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>7.</span> User create new blog, update and delete their own blog</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>8.</span> Users can search blogs by category</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>9.</span> Users can logout</motion.p>
         </div>
       </div>
     </div>
