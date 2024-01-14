963f2e1763f0182cb5b01ab06cdca9d1f76507db
sabbir-62 <sabbir.hossain.se62@gmail.com>
2024-01-14 20:27:42 +0600

    social media project details page update

--- src/components/portfolio/projectDetails/SocialMediaProjectDetails.jsx
+++ src/components/portfolio/projectDetails/SocialMediaProjectDetails.jsx
@@ -29,18 +29,18 @@ const SocialMediaProjectDetails = () => {
       <div className="text-container">
         <div className="key-features">
           <motion.h1 variants={textVariant} initial="initial" whileInView="animate">Key <span>Features</span></motion.h1>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>1.</span> Users can registration and login.</motion.p>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>2.</span> After submitting data for registration, an token sent into user email.</motion.p>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>3.</span> User need to verified this token for completing registration process.</motion.p>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>4.</span> After successfully registration, user need to login.</motion.p>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>5.</span> After login user can see their profile.</motion.p>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>6.</span> Users read others post details.</motion.p>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>7.</span> User create new post, update and delete their own post.</motion.p>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>8.</span> Users can search post by category.</motion.p>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>9.</span> Users can find other users and send friend request.</motion.p>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>10.</span> Users cna accept or delete others request.</motion.p>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>11.</span> Users can comment on others post.</motion.p>
-          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>12.</span> Users can logout.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>1.</span> Users can register and log in.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>2.</span> After submitting data for registration, a token is sent to the user's email.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>3.</span> The user needs to verify this token to complete the registration process.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>4.</span> After successfully registration, the user needs to login.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>5.</span> After logging in, the user can see their profile.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>6.</span> Users read other posts details.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>7.</span> The user can create a new post, update it, and delete their own post.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>8.</span> Users can search posts by category.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>9.</span> Users can find other users and send friend requests.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>10.</span> Users can accept or delete other requests.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>11.</span> Users can comment on others posts.</motion.p>
+          <motion.p variants={textVariant} initial="initial" whileInView="animate"><span>12.</span> Users can log out.</motion.p>
         </div>
       </div>
     </div>
